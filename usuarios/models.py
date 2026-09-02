895c39fc49536d48f7d451e28fccb101098ed689
notzeiro <hectorzutreras@gmail.com>
2026-09-02 14:17:26 -0400

    Configura AUTH_USER_MODEL, corrige modelo Usuario, agrega migracion inicial y limpia __pycache__ del repo

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,39 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-class AbstractUser(AbstractUser.self):
-    run = self.Charfield(parámetros)
-    rol = models.TipoDeCampo(parámetros)
-    fecha_nacimiento
-    direccion=
-    region=
-    comuna=
+class Usuario(AbstractUser):
+
+    class Rol(models.TextChoices):
+        CLIENTE = "cliente", "Cliente"
+        VENDEDOR = "vendedor", "Vendedor"
+        ADMINISTRADOR = "administrador", "Administrador"
+
+    run = models.CharField(max_length=9)
+    rol = models.CharField(max_length=20, choices=Rol.choices, default=Rol.CLIENTE)
+    fecha_nacimiento = models.DateField(null=True , blank=True)
+    direccion = models.CharField(max_length=300)
+    region = models.CharField(max_length=300)
+    comuna = models.CharField(max_length=300)
+
     class Meta:
-        ordering = ['algún_campo']   # opcional: orden por defecto
-        verbose_name = "..."          # opcional: nombre bonito en el admin
+        verbose_name = "Usuario"
 
-    def _str_(self):
-        return self.campo1            # cómo se ve el objeto al imprimirlo
-
-Cosas clave:
-
-Cada clase = una tabla. Cada atributo = una columna.
-_str_ es obligatorio de buena práctica: sin él, el admin de Django y los print() muestran algo como Producto object (1), ilegible.
-Meta es opcional, para configurar comportamiento (orden, nombres).
-Django agrega automáticamente un campo id autoincremental como llave primaria — no hace falta declararlo.
-Tipos de campo que vas a necesitar
-
-
-
-Tipo	Para qué sirve
-CharField(max_length=N)	Texto corto (nombres, títulos) — max_length obligatorio
-TextField()	Texto largo (descripciones, contenido de blog)
-DecimalField(max_digits=N, decimal_places=2)	Precios (nunca uses FloatField para dinero)
-PositiveIntegerField()	Stock, cantidades — no permite negativos
-BooleanField(default=True/False)	El campo activo
-DateTimeField(auto_now_add=True)	Fecha de creación (se pone sola al crear, no editable después)
-DateTimeField(auto_now=True)	Fecha de modificación (se actualiza sola cada vez que guardas)
-ImageField(upload_to='carpeta/')	Imágenes — usa MEDIA_ROOT que ya configuraron
-ForeignKey(OtroModelo, on_delete=models.CASCADE)	Relación "muchos a uno" (ej: un álbum tiene un artista)
-SlugField()	Opcional, útil para URLs bonitas (/productos/master-of-puppets/)
-
-on_delete no es opcional en ForeignKey: define qué pasa si borras el registro relacionado. Lo más común es models.CASCADE
+    def __str__(self):
+        return self.username         # cómo se ve el objeto al imprimirlo
